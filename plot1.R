4c2afb5bf35fc38b265a0a8e4b3df21ffe2575a2
Alex Dudkin <alex_dudkin@yahoo.com>
2015-08-08 18:57:22 +0300

    modified:   plot1.R 	modified:   plot2.R 	modified:   plot3.R 	modified:   plot4.R

--- plot1.R
+++ plot1.R
@@ -6,11 +6,11 @@ f1 <- file("household_power_consumption.txt", open = "rt")
 ptrn <- "^1\\/2\\/2007|^2\\/2\\/2007"
 
 skip1 <- 0
-skip2 <- 0
+bingo <- 0
 
 while (length(ln <- readLines(f1, n=1)) > 0) {
         if (grepl(ptrn, ln)) {
-                skip2 <- 1
+                bingo <- 1
                 break
         }
         skip1 <- skip1+1
@@ -20,17 +20,20 @@ while (length(ln <- readLines(f1, n=1)) > 0) {
         if (!grepl(ptrn, ln)) {
                 break
         }
-        skip2 <- skip2+1
+        bingo <- bingo+1
 }
 close(f1)
 
 
-t1 <- read.csv("household_power_consumption.txt", header = FALSE, sep = ";", skip = skip1, nrows = skip2, na.strings = "?")
+t1 <- read.csv("household_power_consumption.txt", header = FALSE, sep = ";", skip = skip1, nrows = bingo, na.strings = "?")
 h1 <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";", nrows = 1)
 colnames(t1) <- colnames(h1)
 t1$DateTime <- strptime(paste(t1$Date, t1$Time, sep=" "), format = "%d/%m/%Y %H:%M:%S")
 
-png("plot1.png")
+fname <- "plot1.png"
+png(fname, width=480, height=480)
 hist(t1$Global_active_power, freq = TRUE, col="red", main="Global Active Power",xlab="Global Active Power (kilowatts)")
+#fname <- "plot1.png"
+#dev.copy(png, filename=fname, width=480, height=480)
 dev.off()
 
--- plot2.R
+++ plot2.R
@@ -6,11 +6,11 @@ f1 <- file("household_power_consumption.txt", open = "rt")
 ptrn <- "^1\\/2\\/2007|^2\\/2\\/2007"
 
 skip1 <- 0
-skip2 <- 0
+bingo <- 0
 
 while (length(ln <- readLines(f1, n=1)) > 0) {
         if (grepl(ptrn, ln)) {
-                skip2 <- 1
+                bingo <- 1
                 break
         }
         skip1 <- skip1+1
@@ -20,16 +20,17 @@ while (length(ln <- readLines(f1, n=1)) > 0) {
         if (!grepl(ptrn, ln)) {
                 break
         }
-        skip2 <- skip2+1
+        bingo <- bingo+1
 }
 close(f1)
 
-t1 <- read.csv("household_power_consumption.txt", header = FALSE, sep = ";", skip = skip1, nrows = skip2, na.strings = "?")
+t1 <- read.csv("household_power_consumption.txt", header = FALSE, sep = ";", skip = skip1, nrows = bingo, na.strings = "?")
 h1 <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";", nrows = 1)
 colnames(t1) <- colnames(h1)
 t1$DateTime <- strptime(paste(t1$Date, t1$Time, sep=" "), format = "%d/%m/%Y %H:%M:%S")
 
-png("plot2.png")
+fname <- "plot2.png"
+png(fname, width=480, height=480)
 plot(t1$DateTime, t1$Global_active_power, type = "l", xlab = "", ylab="Global Active Power (kilowatts)")
 dev.off()
 
